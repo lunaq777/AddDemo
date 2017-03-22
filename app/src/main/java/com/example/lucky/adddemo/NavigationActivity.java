fab38f4c2128acac89f6c189bb0fc283231745d8
Lachezar Dimitrov <lachezar.id@gmail.com>
2017-03-22 22:51:07 +0200

    Fix main layout & tool bar
    Signed-off-by: Lachezar Dimitrov <lachezar.id@gmail.com>

--- app/src/main/java/com/example/lucky/adddemo/NavigationActivity.java
+++ app/src/main/java/com/example/lucky/adddemo/NavigationActivity.java
@@ -2,12 +2,24 @@ package com.example.lucky.adddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextView mToolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        initGUI();
+    }
+
+    private void initGUI(){
+        mToolbar = (Toolbar) findViewById(R.id.navi_toolbar);
+        mToolbarTitle = (TextView) findViewById(R.id.navi_toolbar_title);
+        mToolbarTitle.setText("Add info");
     }
 }
