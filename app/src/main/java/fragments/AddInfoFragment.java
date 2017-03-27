71bd089274f68e1a3a89816c903ec9a5cfe714c7
Lachezar Dimitrov <lachezar.id@gmail.com>
2017-03-27 20:06:14 +0300

    Add Check for specified price
    Signed-off-by: Lachezar Dimitrov <lachezar.id@gmail.com>

--- app/src/main/java/fragments/AddInfoFragment.java
+++ app/src/main/java/fragments/AddInfoFragment.java
@@ -3,11 +3,14 @@ package fragments;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lucky.adddemo.NavigationActivity;
 import com.example.lucky.adddemo.R;
@@ -28,6 +31,8 @@ public class AddInfoFragment extends Fragment {
     private EditText mTitleText;
     private EditText mDescrText;
     private EditText mPriceText;
+    private final String EMPTY_PRICE = "Not Specified";
+    private final String TOAST_MESSAGE = "You can't leave empty fields";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,10 +51,19 @@ public class AddInfoFragment extends Fragment {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSwitcher.onFragmentSwitch(new PhotoFragment(), PhotoFragment.TITLE, PhotoFragment.TAG);
-                mActivity.addTitle(mTitleText.getText().toString());
-                mActivity.addDescription(mDescrText.getText().toString());
-                mActivity.addPrice(mPriceText.getText().toString());
+                if (TextUtils.isEmpty(mTitleText.getText().toString()) || TextUtils.isEmpty(mDescrText.getText().toString())
+                        || TextUtils.isEmpty(mPriceText.getText().toString())) {
+                    Toast.makeText(getContext(), TOAST_MESSAGE, Toast.LENGTH_SHORT).show();
+                } else {
+                    mSwitcher.onFragmentSwitch(new PhotoFragment(), PhotoFragment.TITLE, PhotoFragment.TAG);
+                    mActivity.addTitle(mTitleText.getText().toString());
+                    mActivity.addDescription(mDescrText.getText().toString());
+                    if (mPriceText.getText().toString().equals("0")) {
+                        mActivity.addPrice(EMPTY_PRICE);
+                    } else {
+                        mActivity.addPrice(mPriceText.getText().toString() + " \u20AC");
+                    }
+                }
             }
         });
         return view;
